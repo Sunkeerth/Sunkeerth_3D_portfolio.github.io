84701ae10aa4c5e371a9f4c0c83c44d6bdbfa2f4
Sunkeerth <sunkeerth123@gmail.com>
2025-10-23 15:39:56 +0530

    Reconnected project after folder move

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // 👈 add this line
+  base: '/sunkeerth-ai-portfolio/', // 👈 add this line
   plugins: [react()],
   server: {
     port: 3000,
